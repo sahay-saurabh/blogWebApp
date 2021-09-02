930d0c390c36c25a61ce6a708df5da6664808893
sahay-saurabh <saurabhj12312312@gmail.com>
2021-09-02 21:47:02 +0530

    connect to database

--- app.js
+++ app.js
@@ -3,23 +3,38 @@
 const express = require("express");
 const ejs = require("ejs");
 const _ =require('lodash');
+const mongoose =require('mongoose');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-const posts=[];
 
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect('mongodb://localhost:27017/blogsDB');
+
+const postSchema={
+  title:String,
+  content:String
+};
+
+const Post=mongoose.model("Post",postSchema);
+
+
 
 
 app.get('/',function(req,res){
-  res.render("home",{homeStartingContent:homeStartingContent,postsArray:posts});
+  Post.find({},function(err,posts){
+    res.render('home',{
+      homeStartingContent:homeStartingContent,
+      posts:posts
+    });
+  });
 });
 
 
@@ -38,30 +53,47 @@ app.get('/publish',function(req,res){
 
 
 app.post('/publish',function(req,res){
- var post={
-   title: req.body.postTitle,
-   text: req.body.postText
- };
+//  var post={
+//    title: req.body.postTitle,
+//    text: req.body.postText
+//  }
+ const post=new Post({
+   title:req.body.postTitle,
+   content: req.body.postText
+ });
 
- posts.push(post);
-
- res.redirect('/');
-
+ post.save(function(err){
+   if(!err){
+     res.redirect('/');
+   }
+ });
 });
 
 
-app.get("/posts/:topic",function(req,res){
+app.get("/posts/:postId",function(req,res){
  
-  posts.forEach(function(post){
-    var temp=post.title;
-    if( _.lowerCase(req.params.topic) == _.lowerCase(post.title)){
-      var para= temp;
-      res.render('post',{
-        title:para,
-        textContent:post.text
+  // posts.forEach(function(post){
+    // var temp=post.title;
+    // if( _.lowerCase(req.params.topic) == _.lowerCase(post.title)){
+    //   var para= temp;
+    //   res.render('post',{
+    //     title:para,
+    //     textContent:post.text
 
-      });
-    }
+    //   });
+    // }
+  // });
+  const requestedPostId=req.params.postId;
+  Post.findOne({_id: requestedPostId}, function(err, post){
+
+    res.render("post", {
+ 
+      title: post.title,
+ 
+      content: post.content
+ 
+    });
+ 
   });
 });
 
